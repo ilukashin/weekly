1025cd5b432e7b4683c4252dc3fb32aeca381a60
gregoryawesoman <ray_couper@mail.ru>
2021-01-29 22:04:54 +0300

    models validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,6 @@
 class Activity < ApplicationRecord
   belongs_to :users
   belongs_to :missions
+
+  validates :duration, presence: true
 end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,6 @@
 class Mission < ApplicationRecord
   belongs_to :users
   has_many :activities
+
+  validates :name, :duration, :description
 end
