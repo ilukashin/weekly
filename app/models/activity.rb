f61793fca37b38c1ddd1f12c3a78102bc9e762a3
gregoryawesoman <ray_couper@mail.ru>
2021-05-24 00:45:58 +0300

    etc

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,6 +5,8 @@ class Activity < ApplicationRecord
   validates :duration, presence: true
   validates :date, presence: true
 
+  after_initialize :init_default_date
+
   def self.to_chart_data(activities)
     return nil if activities.empty?
 
@@ -14,6 +16,12 @@ class Activity < ApplicationRecord
 
   def self.users_last_week(user, date_range)
     user.activities.where(date: date_range)
+  end
+
+  private
+
+  def init_default_date
+    self.date ||= Date.today if new_record?
   end
 
 end
